1270534b58ccd16b6056d2794dc993bc1c062103
Roland DENIS <denis@math.univ-lyon1.fr>
2020-03-06 14:08:06 +0100

    Import Earth splitter by default with tools package

--- src/tools/__init__.py
+++ src/tools/__init__.py
@@ -1,3 +1,4 @@
 from .shapes import create_sphere 
 from .mesh_interfaces import MeshIOInterface, PyMeshInterface, TriMeshInterface
 from .bicolor_sphere import create_bicolor_sphere
+from .tricolor_earth import create_tricolor_earth
